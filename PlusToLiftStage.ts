de6bf066edfcf1a9578b3e3e50a1206713040d0f
Anwesh43 <anweshthecool0@gmail.com>
2019-02-02 17:59:27 +0530

    adding PlusToLift class

--- PlusToLiftStage.ts
+++ PlusToLiftStage.ts
@@ -167,3 +167,25 @@ class PTLNode {
         return this
     }
 }
+
+class PlusToLift {
+    curr : PTLNode = new PTLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+}
