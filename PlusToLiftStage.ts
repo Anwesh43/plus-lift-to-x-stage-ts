66346628fdffa33364c4c4b4b1e9ae2995e5747f
Anwesh43 <anweshthecool0@gmail.com>
2019-02-02 16:54:57 +0530

    adding PlusToLiftStage class

--- PlusToLiftStage.ts
+++ PlusToLiftStage.ts
@@ -50,3 +50,35 @@ const drawPTLNode : Function =  (context : CanvasRenderingContext2D, i : number,
     }
     context.restore()
 }
+
+class PlusToLiftStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#BDBDBD'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : PlusToLiftStage = new PlusToLiftStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+
+}
