2c703eda804d3bfc7a6356ba5dad57d5ecf6c573
Anwesh43 <anweshthecool0@gmail.com>
2019-02-02 18:33:49 +0530

    adding fixes

--- PlusToLiftStage.ts
+++ PlusToLiftStage.ts
@@ -6,7 +6,7 @@ const scGap : number = 0.05
 const scDiv : number = 0.51
 const strokeFactor : number = 90
 const sizeFactor : number = 2.9
-const foreColor : string = '#4527A0'
+const foreColor : string = '#0D47A1'
 
 const maxScale : Function = (scale : number, i : number, n : number) : number => {
     return Math.max(0, scale - i / n)
@@ -21,11 +21,23 @@ const scaleFactor : Function = (scale : number) : number => {
 }
 
 const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
-    return (1 - scaleFactor()) / a + scaleFactor() / b
+    return (1 - scaleFactor(scale)) / a + scaleFactor(scale) / b
 }
 
 const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
     return mirrorValue(scale, a, b) * dir * scGap
+}
+
+const drawRotatedLine : Function = (context : CanvasRenderingContext2D, deg : number, size : number) => {
+    if (size !== 0) {
+        context.save()
+        context.rotate(deg)
+        context.beginPath()
+        context.moveTo(0, 0)
+        context.lineTo(0, size)
+        context.stroke()
+        context.restore()
+    }
 }
 
 const drawPTLNode : Function =  (context : CanvasRenderingContext2D, i : number, scale : number) => {
@@ -33,20 +45,17 @@ const drawPTLNode : Function =  (context : CanvasRenderingContext2D, i : number,
     const size : number = gap / sizeFactor
     const sc1 : number = divideScale(scale, 0, 2)
     const sc2 : number = divideScale(scale, 1, 2)
-    const deg : number = 2 * Math.PI / lines
+    const deg : number = (2 * Math.PI) / lines
     context.lineWidth = Math.min(w, h) / strokeFactor
     context.lineCap = 'round'
     context.strokeStyle = foreColor
     context.save()
     context.translate(w/2, gap * (i + 1))
     for (var j = 0; j < lines; j++) {
-        context.save()
-        context.rotate(j * Math.PI/2 + (deg / 2) * divideScale(sc2, j, lines))
-        context.beginPath()
-        context.moveTo(0, 0)
-        context.lineTo(0, -size * divideScale(sc1, j, lines))
-        context.stroke()
-        context.restore()
+        const rotDeg : number = Math.PI/2 * j
+        const newSize : number = -size * divideScale(sc1, j, lines)
+        const newDeg : number = rotDeg + (deg / 2) * divideScale(sc2, j, lines)
+        drawRotatedLine(context, newDeg, newSize)
     }
     context.restore()
 }
@@ -94,6 +103,7 @@ class State {
 
     update(cb : Function) {
         this.scale += updateValue(this.scale, this.dir, lines, lines)
+        console.log(this.dir)
         if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir
             this.dir = 0
@@ -105,6 +115,7 @@ class State {
     startUpdating(cb : Function) {
         if (this.dir == 0) {
             this.dir = 1 - 2 * this.prevScale
+            console.log(this.dir)
             cb()
         }
     }
@@ -161,7 +172,7 @@ class PTLNode {
 
     getNext(dir : number, cb : Function) : PTLNode {
         var curr : PTLNode = this.next
-        if (dir == 1) {
+        if (dir == -1) {
             curr = this.prev
         }
         if (curr) {
@@ -181,16 +192,16 @@ class PlusToLift {
     }
 
     update(cb : Function) {
-        this.curr.update(cb)
-    }
-
-    startUpdating(cb : Function) {
-        this.curr.startUpdating(() => {
+        this.curr.update(() => {
             this.curr = this.curr.getNext(this.dir, () => {
                 this.dir *= -1
             })
             cb()
         })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
     }
 }
 
